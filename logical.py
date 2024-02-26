bb544cd993940fa313ea6b8d46f3de906fc6c493
Pema Kinzang choden <02230220.cst@rub.edu.bt>
2024-02-25 23:36:20 -0800

    lab3

---
+++ logical.py
@@ -0,0 +1,15 @@
+#AND operator
+#example1
+x = 5
+print(x>3 and x<10) 
+#example2
+y=12
+print(y>10 and y%5==0)
+
+#OR operator
+#example1
+x=5
+print(x<3 or x>10)
+#example2
+y=12
+print(y<10 or y%2==0)
